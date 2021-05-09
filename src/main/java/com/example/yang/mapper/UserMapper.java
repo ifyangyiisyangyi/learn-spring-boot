c52063a0ae4a1fda631cf184c0efd31835604912
ifyangyiisyangyi <yangyi7351598@gmail.com>
2021-05-10 00:17:29 +0800

    test mybatis

---
+++ src/main/java/com/example/yang/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.yang.mapper;
+
+import com.example.yang.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<User> getUserList();
+}
---
+++ src/test/java/com/example/yang/UserMapperTest.java
@@ -0,0 +1,27 @@
+package com.example.yang;
+
+import com.example.yang.mapper.UserMapper;
+import com.example.yang.pojo.User;
+import com.example.yang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserMapperTest {
+    @Test
+    public void test(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        // 方式一
+        UserMapper userDao = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList){
+            System.out.println(user);
+        }
+        // 关闭sqlsession
+        sqlSession.close();
+    }
+}
+
