a4f63b2d08d68a7f705ffdac79fe08e4b6ec1f55
yangyi <yangyi7351598@gmail.com>
2021-12-18 19:36:10 +0800

    up

--- src/main/java/com/example/yang/config/MyMvcConfig.java
+++ src/main/java/com/example/yang/config/MyMvcConfig.java
@@ -13,24 +13,24 @@ import java.util.Locale;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ViewResolver myViewResoler(){
-        return new MyViewResolver();
-    }
-//    @Override   // 处理跨域
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS")
-//                .allowCredentials(true)
-//                .maxAge(3600)
-//                .allowedHeaders("*");
+//    @Bean
+//    public ViewResolver myViewResoler(){
+//        return new MyViewResolver();
 //    }
-
-    public static class MyViewResolver implements ViewResolver{
-        @Override
-        public View resolveViewName(String viewName, Locale locale) throws Exception{
-            return null;
-        }
+    @Override   // 处理跨域
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") //设置允许跨域请求的域名
+                .allowedMethods("GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS") //设置允许的方法
+                .allowCredentials(true) //是否允许证书
+                .maxAge(3600) //配置客户端缓存预检请求的响应时间，单位是秒
+                .allowedHeaders("*");
     }
+
+//    public static class MyViewResolver implements ViewResolver{
+//        @Override
+//        public View resolveViewName(String viewName, Locale locale) throws Exception{
+//            return null;
+//        }
+//    }
 }
