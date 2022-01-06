3ff00e48671f93b8894cd9be202ab56ae6a6781d
yangyi <yangyi7351598@gmail.com>
2022-01-06 18:07:50 +0800

    log4j2

--- src/main/java/com/example/yang/conrtrollers/tools/ShowLinkages.java
+++ src/main/java/com/example/yang/conrtrollers/tools/ShowLinkages.java
@@ -2,21 +2,23 @@ package com.example.yang.conrtrollers.tools;
 
 import com.example.yang.pojo.Linkage;
 import com.example.yang.service.LinkagesService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/linkage")
+@Slf4j  // 等同于下面的注释
 public class ShowLinkages {
+//    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ShowLinkages.class);
     @Resource
     LinkagesService linkagesService;
 
     // 列表
     @RequestMapping(value = "/list", method = {RequestMethod.GET}, produces = "application/json;charset=UTF-8")
     public List<Linkage> showLinkages() {
+        log.info("测试log4j2");
         return linkagesService.getLinkages();
 
     }
