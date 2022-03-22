8182c8e0af828070ff3737729790a3f354e58993
ifyangyiisyangyi <yangyi7351598@gmail.com>
2022-03-22 22:50:30 +0800

    add log

--- src/main/java/com/example/yang/conrtrollers/tools/FeishuBot.java
+++ src/main/java/com/example/yang/conrtrollers/tools/FeishuBot.java
@@ -4,11 +4,13 @@ package com.example.yang.conrtrollers.tools;
 import com.alibaba.fastjson.JSONObject;
 import com.example.yang.pojo.Bot;
 import com.example.yang.pojo.ProblemRecord;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/feishu")
 public class FeishuBot {
@@ -18,6 +20,8 @@ public class FeishuBot {
         Map<String,Object> map = new HashMap<String,Object>();
         map.put("challenge",bot.getChallenge());
         jsonObj.putAll(map);
+        log.info(String.valueOf(jsonObj));
+        System.out.println("********************ok***********");
         return jsonObj;
     }
 }
